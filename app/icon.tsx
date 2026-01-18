561ee3b1abb4eea1ffb1ef2eb0b799edf0ce3885
Omar Suliman <khomod14@gmail.com>
2026-01-18 10:32:02 +0200

    gg

--- app/icon.tsx
+++ app/icon.tsx
@@ -2,8 +2,8 @@ import { ImageResponse } from 'next/og';
 
 // Image metadata
 export const size = {
-    width: 32,
-    height: 32,
+    width: 24,
+    height: 24,
 };
 export const contentType = 'image/png';
 
