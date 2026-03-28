0f5ecddf3abafe2eb044ec537bdc501a3bdb3d7e
omar-Suleiman14 <khomod14@gmail.com>
2026-03-28 09:12:28 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    // @ts-expect-error - Expected in Next.js 15
-    allowedDevOrigins: ["localhost", "172.20.10.3"],
-  },
 };
 
 export default nextConfig;
