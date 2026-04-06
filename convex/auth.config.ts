1c35ba0b1de35fa946894f43dbe3cedc8da66d9f
omar <Khomod14&gmail.com>
2026-04-06 13:03:36 +0200

    ad

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -3,7 +3,7 @@ import { AuthConfig } from "convex/server";
 export default {
   providers: [
     {
-      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || 'https://topical-pangolin-13.clerk.accounts.dev',
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || 'https://clerk.chaos.fail',
       applicationID: "convex",
     },
   ],
